cf0b9afd1e34689e3b2e865f914697bc25cabec9
oilymacaroniconstant <154470161+oilymacaroniconstant@users.noreply.github.com>
2024-01-16 12:53:57 +0100

    Did almost everything from minimum, optional
    Yutaro, Tristan

--- lambda-calculus.py
+++ lambda-calculus.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+import string
 
 # tristan was hier
+
 
 class LambdaTerm:
     """Abstract Base Class for lambda terms."""
@@ -29,13 +31,50 @@ class LambdaTerm:
         output = new_string_list[0]
         return output
 
-    def substitute(self, rules):
-        """Substitute values for keys where they occur."""
-        raise NotImplementedError
-
     def reduce(self):
         """Beta-reduce."""
         # loop/recursie met substitutie functie erop toegepast
+        lijst = [self.substitute()]
+        while True:
+            lijst.append(lijst[-1].substitute())
+            if str(lijst[-2]) == str(lijst[-1]):
+                break
+        return lijst[-1]
+
+    def __eq__(self, other):
+        self = str(self.reduce())
+        other = str(other.reduce())
+
+        letters_self = ''
+        letters_other = ''
+
+        database = string.ascii_letters
+        used_letters_self = []
+        used_letters_other = []
+
+        if len(self) != len(other):
+            return False
+
+        for i in range(len(self)):
+            if self[i] not in used_letters_self:
+                if self[i] in database:
+                    letters_self += self[i]
+                    used_letters_self.append(self[i])
+
+        for i in range(len(other)):
+            if other[i] not in used_letters_other:
+                if other[i] in database:
+                    letters_other += other[i]
+                    used_letters_other.append(other[i])
+
+        for i in range(len(used_letters_self)):
+            self = self.replace(letters_self[i], database[i])
+            other = other.replace(letters_other[i], database[i])
+
+        if self == other:
+            return True
+        else:
+            return False
 
 
 class Variable(LambdaTerm):
@@ -44,12 +83,13 @@ class Variable(LambdaTerm):
     def __init__(self, symbol):
         self.symbol = symbol
 
-    def __repr__(self): raise NotImplementedError
+    def __repr__(self):
+        return self.symbol
 
     def __str__(self):
         return self.symbol
 
-    def substitute(self, rules):
+    def substitute(self):
         return self
 
 
@@ -60,33 +100,45 @@ class Abstraction(LambdaTerm):
         self.variable = variable
         self.body = body
 
-    def __repr__(self): raise NotImplementedError
+    def __repr__(self):
+        return f'λ{self.variable.symbol}.{self.body.symbol}'
 
     def __str__(self):
-        return f'λ{self.variable}.{self.body}'
+        return f'λ{self.variable.symbol}.{self.body.symbol}'
 
     def __call__(self, argument): raise NotImplementedError
 
-    def substitute(self, rules): raise NotImplementedError
+    def substitute(self, argument):
+        if self.variable.symbol == self.body.symbol:
+            return argument
+        else:
+            return self.body
 
 
 class Application(LambdaTerm):
     """Represents a lambda term of the form (M N)."""
 
-    def __init__(self, function, argument): raise NotImplementedError
+    def __init__(self, function, argument):
+        self.function = function
+        self.argument = argument
 
-    def __repr__(self): raise NotImplementedError
+    def __repr__(self):
+        return f'({str(self.function)} {str(self.argument)})'
 
-    def __str__(self): raise NotImplementedError
+    def __str__(self):
+        return f'({str(self.function)} {str(self.argument)})'
 
-    def substitute(self, rules): raise NotImplementedError
-
-    def reduce(self): raise NotImplementedError
+    def substitute(self):
+        if isinstance(self.function, Abstraction):
+            return self.function.substitute(self.argument)
+        else:
+            return Application(self.function.substitute(), self.argument)
 
 
 x = Variable('x')
 y = Variable('y')
-x.fromstring()
-abstractie = Abstraction(x, y)
+abstractie = Abstraction(x, x)
+applicatie = Application(abstractie, y)
 
-print(x)
+print(LambdaTerm.fromstring('lx.y q ly.y z')
+      == LambdaTerm.fromstring('x lx.x q'))
